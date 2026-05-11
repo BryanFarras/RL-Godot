41718125422f6e5adf0bfd76e92bb37ed704fae8
LAPTOP-FUT766KP\asus <farrasbryan06@gmail.com>
2026-05-12 06:23:51 +0700

    push 1

---
+++ app/scripts/player_runner.gd
@@ -0,0 +1,59 @@
+extends CharacterBody3D
+
+## Can we move around?
+@export var can_move : bool = true
+## Are we affected by gravity?
+@export var has_gravity : bool = true
+## Can we press to jump?
+@export var can_jump : bool = true
+## Can we hold to run?
+@export var can_sprint : bool = false
+## Can we press to enter freefly mode (noclip)?
+@export var can_freefly : bool = false
+
+@export_group("Speeds")
+## Look around rotation speed.
+@export var look_speed : float = 0.002
+## Normal speed.
+@export var base_speed : float = 3.0
+
+## Acceleration and drag
+@export var acceleration : float = 10.0
+@export var drag : float = 8.0
+
+## Speed of jump.
+@export var jump_velocity : float = 4.5
+## How fast do we run?
+@export var sprint_speed : float = 5.0
+## How fast do we freefly?
+@export var freefly_speed : float = 25.0
+
+@export_group("Input Actions")
+## Name of Input Action to move Left.
+@export var input_left : String = "ui_left"
+## Name of Input Action to move Right.
+@export var input_right : String = "ui_right"
+## Name of Input Action to move Forward.
+@export var input_forward : String = "ui_up"
+## Name of Input Action to move Backward.
+@export var input_back : String = "ui_down"
+## Name of Input Action to Jump.
+@export var input_jump : String = "ui_accept"
+## Name of Input Action to Sprint.
+@export var input_sprint : String = "sprint"
+## Name of Input Action to toggle freefly mode.
+@export var input_freefly : String = "freefly"
+
+var mouse_captured : bool = false
+var look_rotation : Vector2
+var move_speed : float = 0.0
+var freeflying : bool = false
+
+signal runner_tagged
+
+## IMPORTANT REFERENCES
+@onready var head: Node3D = $Head
+@onready var collider: CollisionShape3D = $CollisionShape3D
+
+func _on_runner_tagged() -> void:
+	print("Runner Tagged!")
---
+++ app/scripts/timer.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var timer = $Timer
+@onready var label = $RichTextLabel
+
+var is_finished := false
+
+func _ready():
+    timer.start()
+    update_label()
+    
+func _process(delta):
+    if not is_finished:
+        update_label()
+
+func update_label():
+    if timer.time_left > 0:
+        label.text = "Timer: %.1f" % timer.time_left
+    else:
+        label.text = "Runner Wins!"
+        is_finished = true
+        print("Runner Wins!")
+
+func _on_runner_tagged():
+    timer.stop()
+    label.text = "Tagged!"
+    is_finished = true
+    print("Tagged!")
